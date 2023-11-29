317e02d9e04ae1e5d6f604aac5c5d023f22d9e85
RaulHorta <a040062@ipmaia.pt>
2023-11-29 22:40:55 +0000

    Mudanças :thumbsup:

--- Webcam Knight/Assets/Scripts/Image Processing/BlobDetection.cs
+++ Webcam Knight/Assets/Scripts/Image Processing/BlobDetection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using Unity.Android.Types;
+//using Unity.Android.Types;
 using UnityEngine;
 
 namespace CamKnight
